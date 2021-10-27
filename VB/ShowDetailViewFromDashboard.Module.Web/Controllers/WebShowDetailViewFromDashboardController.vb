08098032786690d136193f743f9ea07fa944789d
Yekaterina Kiseleva (DevExpress) <yekaterina.kiseleva@devexpress.com>
2021-10-27 19:17:46 +0300

    Add the VB example

---
+++ VB/ShowDetailViewFromDashboard.Module.Web/Controllers/WebShowDetailViewFromDashboardController.vb
@@ -0,0 +1,83 @@
+﻿Imports DevExpress.DashboardWeb
+Imports DevExpress.Data.Filtering
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.ExpressApp.Dashboards.Web
+Imports DevExpress.ExpressApp.Web
+Imports DevExpress.ExpressApp.Web.Templates
+Imports DevExpress.Persistent.Base
+Imports ShowDetailViewFromDashboard.Module.BusinessObjects
+Imports System
+
+Namespace ShowDetailViewFromDashboard.Module.Web.Controllers
+	Public Class WebShowDetailViewFromDashboardController
+		Inherits ObjectViewController(Of DetailView, IDashboardData)
+		Implements IXafCallbackHandler
+
+		Private Const HandlerName As String = "WebShowDetailViewFromDashboardController"
+		Private ReadOnly openDetailViewAction As ParametrizedAction
+
+		Protected Overrides Sub OnActivated()
+			MyBase.OnActivated()
+			Dim dashboardViewerViewItem As WebDashboardViewerViewItem = TryCast(View.FindItem("DashboardViewer"), WebDashboardViewerViewItem)
+			If dashboardViewerViewItem IsNot Nothing Then
+				If dashboardViewerViewItem.DashboardDesigner IsNot Nothing Then
+					CustomizeDashboardViewer(dashboardViewerViewItem.DashboardDesigner)
+				End If
+				AddHandler dashboardViewerViewItem.ControlCreated, AddressOf DashboardViewerViewItem_ControlCreated
+			End If
+		End Sub
+		Private Sub DashboardViewerViewItem_ControlCreated(ByVal sender As Object, ByVal e As EventArgs)
+			Dim dashboardViewerViewItem As WebDashboardViewerViewItem = TryCast(sender, WebDashboardViewerViewItem)
+			CustomizeDashboardViewer(dashboardViewerViewItem.DashboardDesigner)
+		End Sub
+		Private Sub CustomizeDashboardViewer(ByVal dashboardControl As ASPxDashboard)
+			Dim callbackManager As XafCallbackManager = DirectCast(WebWindow.CurrentRequestPage, ICallbackManagerHolder).CallbackManager
+			callbackManager.RegisterHandler(HandlerName, Me)
+			Dim widgetScript As String = "function getOid(s, e) {{
+                                        function findMeasure(measure) {{
+                                            return measure.DataMember === 'Oid';
+                                        }}
+                                        if (e.ItemName.includes('gridDashboardItem')) {{
+                                             var axisPoint = e.GetAxisPoint();
+                                             if (!axisPoint) 
+                                                 return null;
+                                             var itemData = e.GetData(),   
+                                                dataSlice = itemData.GetSlice(axisPoint),
+                                                oidMeasure = dataSlice.GetMeasures().find(findMeasure).Id,
+                                                measureValue = dataSlice.GetMeasureValue(oidMeasure);
+                                                {0}
+                                        }}
+                                    }}"
+			dashboardControl.ClientSideEvents.ItemClick = String.Format(widgetScript, callbackManager.GetScript(HandlerName, "measureValue.GetValue()"))
+		End Sub
+		Public Sub ProcessAction(ByVal parameter As String) Implements IXafCallbackHandler.ProcessAction
+			openDetailViewAction.DoExecute(parameter)
+		End Sub
+		Private Sub OpenDetailViewAction_Execute(ByVal sender As Object, ByVal e As ParametrizedActionExecuteEventArgs)
+			Dim oid As Object
+			If Not Guid.TryParse(CStr(e.ParameterCurrentValue), oid) Then
+				Return
+			End If
+			Dim objectSpace As IObjectSpace = Application.CreateObjectSpace(GetType(Contact))
+			Dim contact As Contact = objectSpace.FindObject(Of Contact)(New BinaryOperator(NameOf(contact.Oid), oid))
+			If contact IsNot Nothing Then
+				e.ShowViewParameters.CreatedView = Application.CreateDetailView(objectSpace, contact, View)
+			End If
+		End Sub
+
+		Protected Overrides Sub OnDeactivated()
+			Dim dashboardViewerViewItem As WebDashboardViewerViewItem = TryCast(View.FindItem("DashboardViewer"), WebDashboardViewerViewItem)
+			If dashboardViewerViewItem IsNot Nothing Then
+				RemoveHandler dashboardViewerViewItem.ControlCreated, AddressOf DashboardViewerViewItem_ControlCreated
+			End If
+			MyBase.OnDeactivated()
+		End Sub
+		Public Sub New()
+			openDetailViewAction = New ParametrizedAction(Me, "Dashboard_OpenDetailView", "Dashboard", GetType(String))
+			openDetailViewAction.Caption = "OpenDetailView"
+			openDetailViewAction.SelectionDependencyType = SelectionDependencyType.RequireSingleObject
+			AddHandler openDetailViewAction.Execute, AddressOf OpenDetailViewAction_Execute
+		End Sub
+	End Class
+End Namespace
---
+++ VB/ShowDetailViewFromDashboard.Module.Win/Controllers/WinShowDetailViewFromDashboardController.vb
@@ -0,0 +1,83 @@
+﻿Imports System
+Imports System.Linq
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon.ViewerData
+Imports DevExpress.DashboardWin
+Imports DevExpress.Data.Filtering
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.ExpressApp.Dashboards.Win
+Imports DevExpress.Persistent.Base
+Imports ShowDetailViewFromDashboard.Module.BusinessObjects
+
+Namespace ShowDetailViewFromDashboard.Module.Win.Controllers
+	Public Class WinShowDetailViewFromDashboardController
+		Inherits ObjectViewController(Of DetailView, IDashboardData)
+
+		Private ReadOnly openDetailViewAction As ParametrizedAction
+
+		Public Sub New()
+			openDetailViewAction = New ParametrizedAction(Me, "Dashboard_OpenDetailView", "Dashboard", GetType(String))
+			openDetailViewAction.Caption = "OpenDetailView"
+			openDetailViewAction.SelectionDependencyType = SelectionDependencyType.RequireSingleObject
+			AddHandler openDetailViewAction.Execute, AddressOf OpenDetailViewAction_Execute
+		End Sub
+
+		Private Sub DashboardViewerViewItem_ControlCreated(ByVal sender As Object, ByVal e As EventArgs)
+			Dim dashboardViewerViewItem As WinDashboardViewerViewItem = TryCast(sender, WinDashboardViewerViewItem)
+			AddHandler dashboardViewerViewItem.Viewer.DashboardItemDoubleClick, AddressOf Viewer_DashboardItemDoubleClick
+		End Sub
+
+		Private Function IsGridDashboardItem(ByVal dashboard As Dashboard, ByVal dashboardItemName As String) As Boolean
+			Dim dashboardItem As DashboardItem = dashboard.Items.SingleOrDefault(Function(item) item.ComponentName = dashboardItemName)
+			Return TypeOf dashboardItem Is GridDashboardItem
+		End Function
+
+		Private Shared Function GetOid(ByVal e As DashboardItemMouseActionEventArgs) As String
+			Dim axisPoint As AxisPoint = e.GetAxisPoint()
+			If axisPoint Is Nothing Then
+				Return Nothing
+			End If
+			Dim data As MultiDimensionalData = e.Data.GetSlice(axisPoint)
+			Dim descriptor As MeasureDescriptor = data.GetMeasures().SingleOrDefault(Function(item) item.DataMember = "Oid")
+			Dim measureValue As MeasureValue = data.GetValue(descriptor)
+			Return measureValue.Value.ToString()
+		End Function
+
+		Private Sub Viewer_DashboardItemDoubleClick(ByVal sender As Object, ByVal e As DashboardItemMouseActionEventArgs)
+			Dim dashboard As Dashboard = DirectCast(sender, DashboardViewer).Dashboard
+			Dim oid As Object
+			If (IsGridDashboardItem(dashboard, e.DashboardItemName) AndAlso openDetailViewAction.Enabled.ResultValue AndAlso openDetailViewAction.Active.ResultValue AndAlso Guid.TryParse(GetOid(e), oid)) Then
+				openDetailViewAction.DoExecute(oid)
+			End If
+		End Sub
+
+		Private Sub OpenDetailViewAction_Execute(ByVal sender As Object, ByVal e As ParametrizedActionExecuteEventArgs)
+			Dim oid As Object = e.ParameterCurrentValue
+			Dim objectSpace As IObjectSpace = Application.CreateObjectSpace(GetType(Contact))
+			Dim contact As Contact = objectSpace.FindObject(Of Contact)(New BinaryOperator(NameOf(contact.Oid), oid))
+			If contact IsNot Nothing Then
+				e.ShowViewParameters.CreatedView = Application.CreateDetailView(objectSpace, contact, View)
+			End If
+		End Sub
+
+		Protected Overrides Sub OnActivated()
+			MyBase.OnActivated()
+			Dim dashboardViewerViewItem As WinDashboardViewerViewItem = TryCast(View.FindItem("DashboardViewer"), WinDashboardViewerViewItem)
+			If dashboardViewerViewItem IsNot Nothing Then
+				If dashboardViewerViewItem.Viewer IsNot Nothing Then
+					AddHandler dashboardViewerViewItem.Viewer.DashboardItemDoubleClick, AddressOf Viewer_DashboardItemDoubleClick
+				End If
+				AddHandler dashboardViewerViewItem.ControlCreated, AddressOf DashboardViewerViewItem_ControlCreated
+			End If
+		End Sub
+
+		Protected Overrides Sub OnDeactivated()
+			Dim dashboardViewerViewItem As WinDashboardViewerViewItem = TryCast(View.FindItem("DashboardViewer"), WinDashboardViewerViewItem)
+			If dashboardViewerViewItem IsNot Nothing Then
+				RemoveHandler dashboardViewerViewItem.ControlCreated, AddressOf DashboardViewerViewItem_ControlCreated
+			End If
+			MyBase.OnDeactivated()
+		End Sub
+	End Class
+End Namespace
